73428dbb1ef261c888b492c33c3c84246a510ce1
Cyan <77025112+shadycyan@users.noreply.github.com>
2024-03-19 22:51:16 +0330

    Add new colorschemes

--- init.lua
+++ init.lua
@@ -742,24 +742,35 @@ require('lazy').setup({
       }
     end,
   },
-
+  -- {
+  --   url = "https://codeberg.org/jthvai/lavender.nvim",
+  --   branch = "stable", -- versioned tags + docs updates from main
+  --   lazy = false,
+  --   priority = 1000,
+  -- },
   { -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
-    'folke/tokyonight.nvim',
+
+    --'folke/tokyonight.nvim',
+    -- 'olivercederborg/poimandres.nvim',
+    'binhtran432k/dracula.nvim',
+
     priority = 1000, -- make sure to load this before all the other start plugins
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
-
+      -- vim.cmd.colorscheme 'tokyonight-night'
+      -- vim.cmd.colorscheme 'poimandres'
+      vim.cmd.colorscheme 'dracula'
       -- You can configure highlights by doing something like
-      vim.cmd.hi 'Comment gui=none'
+      -- vim.cmd.hi 'Comment gui=none'
     end,
     opts = {
+      -- disable_background = true,
       transparent = true,
     },
   },
