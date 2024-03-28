799dd6091632d34f68e14b1f9ad1aa9d14e42594
Cyan <77025112+shadycyan@users.noreply.github.com>
2024-03-28 07:19:41 +0330

    Reformat

--- init.lua
+++ init.lua
@@ -155,8 +155,8 @@ vim.opt.cursorline = true
 vim.opt.scrolloff = 10
 
 -- Tabs & Indentation
-vim.opt.tabstop = 4       -- 4 spaces for tabs (prettier default)
-vim.opt.shiftwidth = 4    -- 4 spaces for indent width
+vim.opt.tabstop = 4 -- 4 spaces for tabs
+vim.opt.shiftwidth = 4 -- 4 spaces for indent width
 vim.opt.expandtab = false -- do not expand tab to spaces
 vim.opt.autoindent = true -- copy indent from current line when starting new one
 vim.opt.smartindent = true
@@ -248,7 +248,7 @@ require('lazy').setup({
   --    require('Comment').setup({})
 
   -- "gc" to comment visual regions/lines
-  { 'numToStr/Comment.nvim',    opts = {} },
+  { 'numToStr/Comment.nvim', opts = {} },
 
   -- Here is a more advanced example where we pass configuration
   -- options to `gitsigns.nvim`. This is equivalent to the following Lua:
@@ -283,7 +283,7 @@ require('lazy').setup({
   -- after the plugin has been loaded:
   --  config = function() ... end
 
-  {                     -- Useful plugin to show you pending keybinds.
+  { -- Useful plugin to show you pending keybinds.
     'folke/which-key.nvim',
     event = 'VimEnter', -- Sets the loading event to 'VimEnter'
     config = function() -- This is the function that runs, AFTER loading
@@ -329,7 +329,7 @@ require('lazy').setup({
       { 'nvim-telescope/telescope-ui-select.nvim' },
 
       -- Useful for getting pretty icons, but requires a Nerd Font.
-      { 'nvim-tree/nvim-web-devicons',            enabled = vim.g.have_nerd_font },
+      { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
 
       { 'debugloop/telescope-undo.nvim' },
     },
