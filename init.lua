d5f63076e7679fa91375e5fcc0f7ca829f956ce7
Cyan <77025112+shadycyan@users.noreply.github.com>
2024-09-28 22:10:38 +0330

    add `telescope-undo` package

--- init.lua
+++ init.lua
@@ -370,6 +370,8 @@ require('lazy').setup({
       },
       { 'nvim-telescope/telescope-ui-select.nvim' },
 
+      { 'debugloop/telescope-undo.nvim' },
+
       -- Useful for getting pretty icons, but requires a Nerd Font.
       { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
     },
@@ -415,6 +417,7 @@ require('lazy').setup({
       -- Enable Telescope extensions if they are installed
       pcall(require('telescope').load_extension, 'fzf')
       pcall(require('telescope').load_extension, 'ui-select')
+      pcall(require('telescope').load_extension, 'undo')
 
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
