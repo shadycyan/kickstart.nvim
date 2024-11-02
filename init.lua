7675db8abab97a43520d19cc8f508fb7b77a472e
Cyan <77025112+shadycyan@users.noreply.github.com>
2024-11-02 20:28:00 +0330

    use catppuccin colorscheme

--- init.lua
+++ init.lua
@@ -890,8 +890,9 @@ require('lazy').setup({
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     -- 'maxmx03/dracula.nvim',
-    'rose-pine/neovim',
-    name = 'rose-pine',
+    -- 'rose-pine/neovim',
+    'catppuccin/nvim',
+    name = 'catppuccin',
     lazy = false,
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
@@ -908,6 +909,8 @@ require('lazy').setup({
       vim.cmd.colorscheme 'tokyonight-night'
     end,
     opts = {
+      flavour = 'mocha', -- latte, frappe, macchiato, mocha
+      transparent_background = true,
       styles = {
         transparency = true,
       },
